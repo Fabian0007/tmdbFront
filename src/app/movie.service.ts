757997a68ef6319a58afe99696b6d54f3768283d
fabian0007 <fabian.cano@correounivalle.edu.co>
2017-07-02 02:39:16 +0000

    Basic View

--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -4,24 +4,21 @@ import { Observable } from 'rxjs';
 
 @Injectable()
 export class MovieService {
-
-  private apiKey = "12598c38af2f10ab6066499ccddba71d";
-  private url = "//api.themoviedb.org/3";
+  private url = "//apitmdb.herokuapp.com/";
 
   constructor(private http: Http) {
    }
 
   getPopular(): Observable<any> {
-    var request =`${this.url}/movie/popular?api_key=${this.apiKey}`;
+    var request =`${this.url}/movie/popular`;
     return this.http.get(request)
       .map(response => {
         return response.json().results;
       });
   }
   
-  
   getTop():Observable<any> {
-    var request =`${this.url}/movie/top_rated?api_key=${this.apiKey}`;
+    var request =`${this.url}/movie/top_rated`;
     return this.http.get(request)
       .map(response => {
         return response.json().results;
@@ -29,7 +26,7 @@ export class MovieService {
   }
   
   getLatest():Observable<any> {
-    var request =`${this.url}/movie/latest?api_key=${this.apiKey}`;
+    var request =`${this.url}/movie/latest`;
     return this.http.get(request)
       .map(response => {
         return response.json().results;
@@ -37,15 +34,15 @@ export class MovieService {
   }
   
   getInTheaters():Observable<any> {
-    var request =`${this.url}/movie/now_playing?api_key=${this.apiKey}`;
+    var request =`${this.url}/movie/now_playing`;
     return this.http.get(request)
       .map(response => {
         return response.json().results;
       });
   }
   
-    getUpComing():Observable<any> {
-    var request =`${this.url}/movie/upcoming?api_key=${this.apiKey}`;
+  getUpComing():Observable<any> {
+    var request =`${this.url}/movie/upcoming`;
     return this.http.get(request)
       .map(response => {
         return response.json().results;
@@ -53,16 +50,15 @@ export class MovieService {
   }
   
   getMovie(id:number): Observable<any> {
-    var request =`${this.url}/movie/${id}?api_key=${this.apiKey}&append_to_response=alternative_titles,credits,releases,videos,similar,reviews,images`;
+    var request =`${this.url}/movie/${id}`;
     return this.http.get(request)
       .map(response => {
         return response.json();
       });
   }
   
-  
-    getPerson(id:number): Observable<any> {
-    var request =`${this.url}/person/${id}?api_key=${this.apiKey}&append_to_response=alternative_titles,credits,releases,videos,similar,reviews,images`;
+  getPerson(id:number): Observable<any> {
+    var request =`${this.url}/person/${id}`;
     return this.http.get(request)
       .map(response => {
         return response.json();
@@ -73,36 +69,32 @@ export class MovieService {
     if ( page === undefined ) {
       page = 1;
     }
-    var request =`${this.url}/search/multi?api_key=${this.apiKey}&query=${search}&page=${page}&include_adult=false`;
+    var request =`${this.url}/search/multi/${search}/${page}`;
     return this.http.get(request)
       .map(response => {
         return response.json().results;
       });
   }
   
-  
   getSearchPerson(search:string, page:number): Observable<any> {
     if ( page === undefined ) {
       page = 1;
     }
-    var request =`${this.url}/search/person?api_key=${this.apiKey}&query=${search}&page=${page}&include_adult=false`;
+    var request =`${this.url}/search/person/${search}/${page}`;
     return this.http.get(request)
       .map(response => {
         return response.json();
       });
   }
-  
   
   getSearchMovie(search:string, page:number): Observable<any> {
     if ( page === undefined ) {
       page = 1;
     }
-    var request =`${this.url}/search/movie?api_key=${this.apiKey}&query=${search}&page=${page}&include_adult=false`;
+    var request =`${this.url}/search/movie/${search}/${page}`;
     return this.http.get(request)
       .map(response => {
         return response.json();
       });
   }
-  
-  
 }
